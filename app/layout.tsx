14996d0f380cd281f2a449228e12ba57c10f4713
efimeriosagiasvarvaras-crypto <efimeriosagiasvarvaras@gmail.com>
2026-04-01 22:58:52 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Meteo Αγριάς",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="el">
+      <body>{children}</body>
+    </html>
+  );
+}
